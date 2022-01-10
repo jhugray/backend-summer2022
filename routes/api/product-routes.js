8d8af5829f3052a3a49742c52d3e0d19e883ef5a
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-10 13:54:35 -0500

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,15 +16,73 @@ router.get('/', (req, res) => {
 });
 
 // GET /api/products/1 (1 = example id#)
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+  Product.findOne()
+  .then(dbProductData => {
+    //if no product with that id, error message returned
+    if (!dbProductData) {
+      res.status(404).json({ message:'No product found with that ID' })
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
+});
 
 // POST /api/products
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  Product.create(req.body)
+  .then((dbProductData) => {
+    res.status(200).json(dbProductData)
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
 
 // PUT /api/products/1 (1 = example id#)
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+  Product.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    //if no product with that id, error message returned
+    if (!dbProductData[0]) {
+      res.status(404).json({message: 'No product found with that ID'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 // DELETE /api/products/1 (1 = example id#)
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({message: 'No product found with that ID'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
