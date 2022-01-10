72a7186e4c136a21306638c49f0929782046ee35
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-10 11:28:36 -0500

    product findall GET route added, config updated

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
-  port: 3001
 });
 
 module.exports = sequelize;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,7 +2,18 @@ const router = require('express').Router();
 const { Product } = require('../../models');
 
 // GET /api/products
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  //runs the findAll method on the Product model
+  Product.findAll()
+  //returns data in JSON format
+  .then(dbProductData => res.json(dbProductData))
+  //error handling
+  .catch(err => {
+    console.log(err);
+    // internal server error
+    res.status(500).json(err)
+  });
+});
 
 // GET /api/products/1 (1 = example id#)
 router.get('/:id', (req, res) => {});
