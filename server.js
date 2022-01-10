77fb80002dd8686f461432c22e40d1c2dd3d6128
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-10 09:51:11 -0500

    model folder setup, server set up

---
+++ server.js
@@ -0,0 +1,25 @@
+//import express
+const express = require('express');
+//import (CRUD RESTful API) routes
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+//server at PORT 3001 (or any port #) using express. 
+//Credentials hidden in a .env file that does not get pushed to github as it is in the .gitignore file
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Express middleware to parse the Request Object
+//parses JSON object
+app.use(express.json());
+//parses strings or arrays
+app.use(express.urlencoded({ extended: true }));
+
+//use the API routes imported above
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
