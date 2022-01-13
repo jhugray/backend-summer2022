2942c3207b233d7085606b30924888d41802ba0a
Jess Hause Ugray <jess.ugray@gmail.com>
2022-01-13 10:52:04 -0500

    csv downlad version 1

--- public/js/export.js
+++ public/js/export.js
@@ -10,9 +10,26 @@ async function exportProductData (event) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data);
+
+  
+      // TO DO: Add HEADER, parse category object
+      let csvContent = "data:text/csv;charset=utf-8,";
+      let csvData = data.map(obj => Object.values(obj));
+
+      csvData.forEach(function(rowArray) {
+          let row = rowArray.join(",");
+          csvContent += row + "\r\n";
+      });
+
+      console.log(csvContent);
+      // var encodedUri = encodeURI(csvContent);
+      // window.open(encodedUri);
+
+
       })
     }
   })
+
 };
 
 document.querySelector('.exportCSV').addEventListener('click', exportProductData);
